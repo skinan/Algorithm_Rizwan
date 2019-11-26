2b8108ec721de1f3c3b54abf2bde9e594b5f43c7
Sakib Khan Inan <sakib.khaninan@gmail.com>
2019-11-26 16:12:16 +0600

    Update longest_common_subsequence_(lcs).py

--- longest_common_subsequence_(lcs).py
+++ longest_common_subsequence_(lcs).py
@@ -1,70 +1,67 @@
-# longest common subsequence algorithm
+# !usr/bin/env/python3
+# Longest Common Subsequence Algorithm
 # Dynamic Programming Approach
+# Worst Case Run Time Complexity : O(n^2)
 
 
-def longest_common_subsequence(x, y):
+def longest_common_subsequence(main_string, comparing_string):
 
-    m = len(x)  # Get the length of the first word or base word
-    n = len(y)  # Get the length of the second word or comparing word
+    columns_length = len(main_string)  # Get the length of the first word or base word
+    rows_length = len(comparing_string)  # Get the length of the second word or comparing word
 
     # MAKE A 2D LIST (MATRIX)
-    loc = [[None] * (m + 1) for i in range(n + 1)]
+    dynamic_table = [[0] * (columns_length + 1) for i in range(rows_length + 1)]
 
-    # n = NUMBER OF ROWS
-    # m = NUMBER OF COLUMNS
-
-    # MAKE THE 1ST ROW AND COLUMN FILL WITH ALL 0's.
-    for i in range(n + 1):
-        for j in range(m + 1):
-            if i == 0 or j == 0:
-                loc[i][j] = 0
-
+    # rows_length = NUMBER OF ROWS
+    # columns_length = NUMBER OF COLUMNS
+    
     # FILL THE MATRIX FOLLOWING LCS ALGORITHM.
-    for i in range(1, n + 1):
-        for j in range(1, m + 1):
-            if x[j - 1] == y[i - 1]:
-                loc[i][j] = 1 + int(loc[i - 1][j - 1])
+    for i in range(1, rows_length + 1):
+        for j in range(1, columns_length + 1):
+            if main_string[j - 1] == comparing_string[i - 1]:
+                dynamic_table[i][j] = 1 + dynamic_table[i - 1][j - 1]
 
             else:
-                loc[i][j] = max(loc[i - 1][j], loc[i][j - 1])
+                dynamic_table[i][j] = max(dynamic_table[i - 1][j], dynamic_table[i][j - 1])
 
     print("MATRIX ACCORDING TO LONGEST COMMON SUBSEQUENCE ALGORITHM: \n ")
 
-    for i in range(n + 1):
-        print(loc[i])
+    for i in range(rows_length + 1):
+        print(dynamic_table[i])
 
-    print("\n\n")
-    print("LENGTH OF LONGEST COMMON SUBSEQUENCE = ", loc[n][m])
-    print("\n\n")
+    print()
+    print("LENGTH OF LONGEST COMMON SUBSEQUENCE = ", dynamic_table[rows_length][columns_length])
+    print()
 
-    i = len(y)
-    j = len(x)
-    lcs = ""
+    i = len(comparing_string)
+    j = len(main_string)
+
+    lcs_string = str()
 
     # BACKTRACKING TO FIND THE LONGEST COMMON SUBSEQUENCE
 
     temp = True
 
     while temp is True:
-        if loc[i][j] == 0:
+        if dynamic_table[i][j] == 0:
             temp = False
-        elif loc[i][j] == loc[i][j - 1]:
+        elif dynamic_table[i][j] == dynamic_table[i][j - 1]:
             j = j - 1
 
         else:
-            lcs = x[j-1] + lcs
+            lcs_string = main_string[j-1] + lcs_string
             i = i - 1
             j = j - 1
 
-    return lcs
+    return lcs_string
 
 
 def main():
-    x = "longest"  # x = MAIN WORD
-    y = "stone"  # y = COMPARING WORD
+    main_string = "longest"  # main_string === MAIN WORD
+    comparing_string = "stone"  # comparing_string === COMPARING WORD
 
     # CALLING THE FUNCTION
-    print("THE LCS WORD IS: ", longest_common_subsequence(x, y))
+    print("THE LCS WORD IS: ", longest_common_subsequence(main_string, comparing_string))
 
 
 if __name__ == '__main__':
